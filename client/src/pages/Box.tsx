712104240d5632f4aea732559b51e9eac40f3df9
maryalexissolis <45756139-maryalexissolis@users.noreply.replit.com>
2025-08-05 10:38:10 +0000

    Initial commit

---
+++ client/src/pages/Box.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Box = (): JSX.Element => {
+  return (
+    <div className="w-full min-h-screen bg-[#f3f1ec]">
+      <div className="fixed inset-0 bg-[#f3f1ec] -z-10" />
+    </div>
+  );
+};
